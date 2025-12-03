a1381b0ffa88bd4379c72c4787d4d1e92198aa74
dakeqi <1265667788@qq.com>
2025-12-03 10:35:52 +0800

    Fix: disable LED to prevent GPIO_NUM_NC assertion failure

--- esp32c3_supermini_board.cc
+++ esp32c3_supermini_board.cc
@@ -93,8 +93,9 @@ public:
     }
 
     virtual Led* GetLed() override {
-        static SingleLed led(BUILTIN_LED_GPIO);
-        return &led;
+        // ESP32-C3 SuperMini板载LED(GPIO8)已用于显示屏DC引脚
+        // 返回nullptr禁用LED功能
+        return nullptr;
     }
 
     virtual Display* GetDisplay() override {
